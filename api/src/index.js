63fd64d46cf5bfa1024724836d729ab6a23f45f4
Kaleb <Kaleb@Kaleb.com>
2026-01-01 05:40:06 +0800

    fix: export app instance in index.js for Azure Functions v4

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,12 @@
-// Import all functions
+// Azure Functions v4 Programming Model
+// All functions are automatically registered when imported
+const { app } = require('@azure/functions');
+
+// Import all function definitions
 require('./functions/getMedia');
 require('./functions/uploadMedia');
 require('./functions/updateMedia');
 require('./functions/deleteMedia');
 
-
+// Export the app instance for Azure Functions runtime
+module.exports = app;
