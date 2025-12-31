23e11637bd940d2c8f8e93f28768f47486d127ad
Kaleb <Kaleb@Kaleb.com>
2026-01-01 05:45:43 +0800

    fix: simplify index.js for Azure Functions v4 auto-discovery

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,6 @@
 // Azure Functions v4 Programming Model
-// All functions are automatically registered when imported
-const { app } = require('@azure/functions');
-
-// Import all function definitions
+// Functions are automatically discovered and registered
 require('./functions/getMedia');
 require('./functions/uploadMedia');
 require('./functions/updateMedia');
 require('./functions/deleteMedia');
-
-// Export the app instance for Azure Functions runtime
-module.exports = app;
